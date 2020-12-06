4745b1f56bfe715a347e096af5e7d402cabef1a7
jbrad88 <jodybradley@gmail.com>
2020-12-06 17:53:32 +0000

    Further work done on HTML/JSON. Page now outputs power prediction using speed value per function. Need to build so that user can input speed value

--- server.py
+++ server.py
@@ -7,39 +7,39 @@ import sklearn.linear_model as lin
 import pandas as pd
 import seaborn as sns
 
-# Create a new web app.
-app = fl.Flask(__name__)
-
-#testing if live...
-@app.route('/')
-def index():
-    return "hello"
-
-# Add root route.
-@app.route("/power")
-def f(x, p):
-    return p[0] + x * p[1]
-
+# Loan our data set.
 df = pd.read_csv('powerproduction.csv')
 col=['speed','power']
 
 # Take the two variables from our data set.
 production = df[["speed", "power"]].dropna()
-# Scatter and fit line for just those two variables.
-sns.regplot(x="speed", y="power", data=df)
 
+# Train the data set.
 x = production["speed"].to_numpy()
 y = production["power"].to_numpy()
-
 x = x.reshape(-1, 1)
-
 model = lin.LinearRegression()
 model.fit(x, y)
-
 r = model.score(x, y)
 p = [model.intercept_, model.coef_[0]]
+
+def f(x, p):
+    return p[0] + x * p[1]
 
 def predict(x, p):
     return f(x, p)
 
-print(predict(15,p))
+# Create a new web app.
+app = fl.Flask(__name__)
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+@app.route("/hello")
+def answer():
+    return {"value": predict(16, p)}
+
+
+# References
+# [1] https://stackoverflow.com/questions/20646822/how-to-serve-static-files-in-flask
